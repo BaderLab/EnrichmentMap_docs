5cc30ecd05188edff3f32c81553d9f700984c9d3
Mike Kucera <mikekucera@gmail.com>
2018-01-25 13:36:47 -0500

    update logo

--- conf.py
+++ conf.py
@@ -87,7 +87,7 @@ todo_include_todos = False
 import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_logo = 'images/enrichmentmap_logo_mini.png'
+html_logo = 'images/enrichmentmap_logo_mini_bg.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
